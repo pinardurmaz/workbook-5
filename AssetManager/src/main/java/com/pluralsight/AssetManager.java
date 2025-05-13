ea3f416eb48b7585121ead64b02d7a2ee6ed04f6
Pinar Durmaz <pnr.drmz02@gmail.com>
2025-05-13 15:45:01 -0400

    Completed AssetManager

--- AssetManager/src/main/java/com/pluralsight/AssetManager.java
+++ AssetManager/src/main/java/com/pluralsight/AssetManager.java
@@ -5,19 +5,19 @@ public class AssetManager {
     public static void main(String[] args) {
         ArrayList<Asset> myAssets = new ArrayList<>();
 
-        // Evler
+        // Houses
         myAssets.add(new House("My House", "2020-05-01", 250000,
                 "123 Maple Street", 1, 1800, 5000));
         myAssets.add(new House("Vacation Home", "2018-07-10", 350000,
                 "456 Lakeview Drive", 2, 2200, 8000));
 
-        // Araçlar
+        // Vehicles
         myAssets.add(new Vehicle("Tom's Truck", "2015-06-15", 30000,
                 "Ford F-150", 2015, 120000));
         myAssets.add(new Vehicle("My Car", "2022-01-20", 25000,
                 "Honda Civic", 2022, 45000));
 
-        // Listeleme
+        // List
         for (Asset asset : myAssets) {
             System.out.println("Description: " + asset.getDescription());
             System.out.println("Date Acquired: " + asset.getDateAcquired());
